3892259ee2f52b7dffc0ae3d7fdc97e98f280262
yamashun <yshunske@gmail.com>
2023-05-03 23:09:14 +0900

    add nonce column

--- db/migrate/20230417123829_create_requests.rb
+++ db/migrate/20230417123829_create_requests.rb
@@ -2,7 +2,8 @@ class CreateRequests < ActiveRecord::Migration[7.0]
   def change
     create_table :requests do |t|
       t.string :request_id
-      t.string :state
+      t.text :state
+      t.text :nonce
       t.references :client, null: false, foreign_key: true
       t.datetime :expires_at
 
